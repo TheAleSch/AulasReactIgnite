cc7e79f755ffd13b32f260f89f0b57edccc9168d
Alexandre Schrammel <alexandre.schrammel@gmail.com>
2023-09-17 01:45:07 -0300

    breaking components to improve manteinability

--- 02-timer/src/pages/Home/styles.ts
+++ 02-timer/src/pages/Home/styles.ts
@@ -15,17 +15,7 @@ export const HomeContainer = styled.main`
     gap: 3.5rem;
   }
 `;
-export const FormContainer = styled.div`
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 0.5rem;
-  color: ${(props) => props.theme["gray-100"]};
-  font-size: 1.125rem;
-  font-weight: bold;
-  flex-wrap: 1rem;
-`;
+
 export const CountdownContainer = styled.div`
   font-family: "Roboto Mono", monospace;
   font-size: 10rem;
@@ -49,35 +39,6 @@ export const Separator = styled.div`
   display: hidden;
   display: flex;
   justify-content: center;
-`;
-
-export const BaseInput = styled.input`
-  background: transparent;
-  height: 2.5rem;
-  border: 0;
-  border-bottom: 2px solid ${(props) => props.theme["gray-500"]};
-  font-weight: bold;
-  font-size: 1.125rem;
-  padding: 0 0.5rem;
-  color: ${(props) => props.theme["gray-100"]};
-  &:focus {
-    box-shadow: none;
-    border-color: ${(props) => props.theme["green-500"]};
-  }
-  &::placeholder {
-    color: ${(props) => props.theme["gray-500"]};
-  }
-`;
-
-export const TaskInput = styled(BaseInput)`
-  flex: 1;
-`;
-
-export const MinutesAmountInput = styled(BaseInput)`
-  width: 4rem;
-  &::-webkit-calendar-picker-indicator {
-    display: none !important;
-  }
 `;
 
 export const BaseCountdownButton = styled.button`
